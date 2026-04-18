12c07ff4de0930379d8ef55a42e83ad3bf43d20a
Hiten1507 <hitenmanchanda15@gmail.com>
2026-04-18 23:33:36 +0530

    Refactor assembly code for node management

--- q1/q1.s
+++ q1/q1.s
@@ -6,111 +6,120 @@
 
 make_node:
     addi sp, sp, -16
-    sd x1, 8(sp)
-    sd x11, 0(sp)
+    sd ra, 8(sp)
+    sd a1, 0(sp)
 
     li a0, 24
     call malloc
+    beq a0, x0, make_fail
 
-    ld x11, 0(sp)
-    sw x11, 0(a0)
+    ld a1, 0(sp)
+    sw a1, 0(a0)
     sd x0, 8(a0)
     sd x0, 16(a0)
 
-    mv x10, a0
-    ld x1, 8(sp)
+    ld ra, 8(sp)
+    addi sp, sp, 16
+    ret
+
+make_fail:
+    li a0, 0
+    ld ra, 8(sp)
     addi sp, sp, 16
     ret
 
 insert:
     addi sp, sp, -24
-    sd x1, 16(sp)
-    sd x10, 8(sp)
-    sd x11, 0(sp)
+    sd ra, 16(sp)
+    sd a0, 8(sp)
+    sd a1, 0(sp)
 
-    beq x10, x0, ins_new
+    beq a0, x0, ins_new
 
-    lw x5, 0(x10)
-    blt x11, x5, ins_left
-    bgt x11, x5, ins_right
+    lw t0, 0(a0)
+    blt a1, t0, ins_left
+    bgt a1, t0, ins_right
     j ins_done
 
 ins_new:
-    ld x11, 0(sp)
-    mv x10, x11
+    ld a1, 0(sp)
     call make_node
     j ins_exit
 
 ins_left:
-    ld x10, 8(x10)
-    ld x11, 0(sp)
+    ld t1, 8(a0)
+    mv a0, t1
+    ld a1, 0(sp)
     call insert
-    mv x6, x10
-    ld x10, 8(sp)
-    sd x6, 8(x10)
+
+    mv t2, a0
+    ld a0, 8(sp)
+    sd t2, 8(a0)
     j ins_done
 
 ins_right:
-    ld x10, 16(x10)
-    ld x11, 0(sp)
+    ld t1, 16(a0)
+    mv a0, t1
+    ld a1, 0(sp)
     call insert
-    mv x6, x10
-    ld x10, 8(sp)
-    sd x6, 16(x10)
+
+    mv t2, a0
+    ld a0, 8(sp)
+    sd t2, 16(a0)
     j ins_done
 
 ins_done:
-    ld x10, 8(sp)
+    ld a0, 8(sp)
 
 ins_exit:
-    ld x1, 16(sp)
+    ld ra, 16(sp)
     addi sp, sp, 24
     ret
 
 get:
-    beq x10, x0, get_null
+    beq a0, x0, get_null
 
-    lw x5, 0(x10)
-    beq x5, x11, get_found
-    blt x11, x5, get_left
+    lw t0, 0(a0)
+    beq t0, a1, get_found
+    blt a1, t0, get_left
 
-    ld x10, 16(x10)
+    ld a0, 16(a0)
     j get
 
 get_left:
-    ld x10, 8(x10)
+    ld a0, 8(a0)
     j get
 
 get_found:
     ret
 
 get_null:
-    li x10, 0
+    li a0, 0
     ret
 
 getAtMost:
-    li x7, -1
+    li t2, -1
 
 loop:
-    beq x11, x0, done
+    beq a1, x0, done
 
-    lw x5, 0(x11)
+    lw t0, 0(a1)
 
-    beq x5, x10, exact
-    bgt x5, x10, go_left
+    beq t0, a0, exact
+    bgt t0, a0, go_left
 
-    mv x7, x5
-    ld x11, 16(x11)
+    mv t2, t0
+    ld a1, 16(a1)
     j loop
 
 go_left:
-    ld x11, 8(x11)
+    ld a1, 8(a1)
     j loop
 
 exact:
-    mv x10, x5
+    mv a0, t0
     ret
 
 done:
-    mv x10, x7
+    mv a0, t2
     ret
